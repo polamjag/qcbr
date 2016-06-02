fb27e4a03db9f58adce6958e36b9b24f11c5f461
polamjag <s@polamjag.info>
2016-06-02 10:55:45 +0900

    add spec to test string encoding

--- spec/qcbr_spec.rb
+++ spec/qcbr_spec.rb
@@ -5,7 +5,7 @@ describe Qcbr do
     expect(Qcbr::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'can encode string to specified format' do
+    expect(Qcbr.encode_text('hogeあ亜👽').unpack('h*')).to eq ['00:00:00:68:00:00:00:6f:00:00:00:67:00:00:00:65:00:00:30:42:00:00:4e:9c:00:01:f4:7d'.gsub(':', '')]
   end
 end
